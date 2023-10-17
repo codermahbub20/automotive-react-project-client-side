3b1c52154022909bfaf94328865039508335bd57
codermahbub20 <mkrefat5@gmail.com>
2023-10-18 00:08:02 +0600

    Nvbar created

--- src/Shared/Navbar/NavBar.jsx
+++ src/Shared/Navbar/NavBar.jsx
@@ -1,34 +1,36 @@
-import { Link } from "react-router-dom";
+
+import { NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
 
-    const routeLinks = <>
-        <Link><li></li></Link>
+    const navLinks = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/jobs">Add Product</NavLink></li>
+        <li><NavLink to="/applied">Add Cart</NavLink></li>
+        <li><NavLink to="/blogs">Login</NavLink></li>
     </>
 
     return (
-        <div>
-            <div className="navbar bg-red-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            ..
-                        </ul>
-                    </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                       
+        <div className="navbar bg-black">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLinks}
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
-                </div>
+                <img className="w-32 h-10" src="https://demo.vehica.com/wp-content/uploads/2021/09/vehica-logo-white-retina.png" alt="" />
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
             </div>
         </div>
     );
